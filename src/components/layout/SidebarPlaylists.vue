3c57e2beb1274a67a1ee3a2a62cfa17d861c07a2
Billy636 <2889271550@qq.com>
2026-07-01 20:49:54 +0800

    style: 优化侧边栏歌单布局，改用双行名称并隐藏歌曲数量与删除按钮

--- src/components/layout/SidebarPlaylists.vue
+++ src/components/layout/SidebarPlaylists.vue
@@ -81,10 +81,8 @@ const getPlaylistCover = (playlistId: string) => {
               <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 dark:text-white/40" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" /></svg>
             </div>
             <div class="flex-1 min-w-0 flex flex-col justify-center">
-              <span class="text-sm truncate leading-tight mb-0.5">{{ list.name }}</span>
-              <span class="text-[10px] text-gray-600 dark:text-gray-300 leading-tight">{{ list.songPaths.length }} 首</span>
+              <span class="text-xs line-clamp-2 break-all leading-tight">{{ list.name }}</span>
             </div>
-            <button @click.stop="$emit('deletePlaylist', list.id, list.name)" class="absolute right-2 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 text-gray-400 dark:text-white/60 hover:text-red-500 transition-all p-1" title="删除歌单"><svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg></button>
           </li>
         </TransitionGroup>
       </ul>
